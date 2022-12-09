89c71abdb88c7b8a3d9b809248e4eaec68de821b
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-12-09 18:50:17 +0300

    v1.0

--- notification_test/yasg.py
+++ notification_test/yasg.py
@@ -17,8 +17,8 @@ schema_view = get_schema_view(
 )
 
 swaggerurlpatterns = [
-   re_path(r'^docs/$',
-           schema_view.with_ui('swagger', cache_timeout=0),
-           name='schema-swagger-ui'
-           ),
+    re_path(r'^docs/$',
+            schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'
+            ),
 ]
